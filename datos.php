c28893860a4b473cdd7394bcea83a4abc7da5944
Belén Izquierdo dorado <59977724+bnizquierdo@users.noreply.github.com>
2022-01-22 23:02:52 +0100

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,4 @@
+Calle: <?php echo htmlspecialchars($_POST['calle']); ?> <br/>
+Número: <?php echo  (int)($_POST['numero'])  ;?> <br/>
+Población: <?php echo htmlspecialchars($_POST['poblacion']); "<br/>";?> <br/>
+País: <?php echo htmlspecialchars($_POST['pais']); "<br/>";?> <br/>
